342b381ae56e29a8cf96219d4d31c5d28faf507e
José Fonte <73178551+josefonte@users.noreply.github.com>
2023-01-07 19:40:12 +0000

    Update TextUI.java

--- FASE 3 - Implementação/RacingManager/src/ui/TextUI.java
+++ FASE 3 - Implementação/RacingManager/src/ui/TextUI.java
@@ -1,9 +1,3 @@
-/*
- *  DISCLAIMER: Este código foi criado para discussão e edição durante as aulas práticas de DSS, representando
- *  uma solução em construção. Como tal, não deverá ser visto como uma solução canónica, ou mesmo acabada.
- *  É disponibilizado para auxiliar o processo de estudo. Os alunos são encorajados a testar adequadamente o
- *  código fornecido e a procurar soluções alternativas, à medida que forem adquirindo mais conhecimentos.
- */
 
 import java.util.ArrayList;
 import java.util.List;
